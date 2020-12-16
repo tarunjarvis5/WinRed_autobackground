bd40b7237d5c2d2a462766cfa5241c6437b18530
Tarun Nair <tarunnair200029@gmail.com>
2020-12-16 11:31:21 +0530

    option optimization

--- run.py
+++ run.py
@@ -3,9 +3,18 @@ import redditbackground
 
 print("Enter subredditname :")
 subredditName = input()
+
 page = input("Select the subreddit filter (Hot,Top,or New) : ")
 page = page.lower()
+if page not in ["hot","top","new"] :
+    sys.exit("Input****Error****")
+
 save = input("Do you want to download and save the image? (y/n) : ")
+if save not in "yn":
+    sys.exit("Input****Error****")
+elif save == "yn" :
+    sys.exit("Input****Error****")
+
 print("Setting your background as the top image of /r/" + subredditName + "...")
 image = redditbackground.setBackgroundFromSubreddit(subredditName,page,save)
 print("Done!")
